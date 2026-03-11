ffd1121070455ce7b8c020d6f2ffcc7c0d148b8f
wii <wii4winnie@163.com>
2026-03-11 19:30:42 +0800

    i

--- lib/local/model/plan.dart
+++ lib/local/model/plan.dart
@@ -1,4 +1,7 @@
 import 'package:isar/isar.dart';
+import 'package:music/common/models/common_enum.dart';
+import 'package:music/common/models/label_type.dart';
+import 'package:music/common/models/plan_type.dart';
 part 'plan.g.dart';
 
 @collection
@@ -8,4 +11,16 @@ class Plan {
   late String description;
   late DateTime date;
   late int timeInMinutes;
+  @enumerated
+  late PlanLabel label;
+  late int colorValue;
+  @enumerated
+  late IntervalEnum interval;
+  late double intervalHour;
+  late String? note;
+  @enumerated
+  late PlanStatus status;
+  late int? finishCount;
+  late DateTime createTime;
+  late DateTime lastFinishedTime;
 }
